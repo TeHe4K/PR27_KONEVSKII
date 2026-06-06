d2cc73c96fc41f4ed0a35846b6f4fadfec760feb
TeHe4K <hryblackwii@gmail.com>
2026-06-07 01:01:29 +0500

    Добавьте файлы проекта.

---
+++ Cinema_Konevskii/Models/Kinoteatr.cs
@@ -0,0 +1,18 @@
+﻿namespace Cinema_Konevskii.Models
+{
+    public class Kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set;  }
+        public int Count { get; set;  }
+
+        public Kinoteatr(int Id, string Name,int CountZal, int Count)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+    }
+}
