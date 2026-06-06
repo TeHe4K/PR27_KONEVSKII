6e63bdeb0642f11a04a9b012bec17510e794dd4f
TeHe4K <hryblackwii@gmail.com>
2026-06-07 01:09:44 +0500

    Разработан класс для работы с БД. Коневский

---
+++ Cinema_Konevskii/Classes/Common/Connection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Cinema_Konevskii.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=127.0.0.1;uid=root;pwd=;database=cinema";
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL,connection).ExecuteReader();
+        }
+        public void CloseConection(MySqlConnection connection) 
+        {
+            connection.Close();
+            MySqlConnection.ClearAllPools();
+        }
+    }
+}
